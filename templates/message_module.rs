65699663881ba2eb64a6c0bf9757324b4543f0d8
Nathan Bergey <nathan.bergey@gmail.com>
2017-12-31 17:38:24 -0700

    Codegen: add messages module helpers 'to_csv' and 'to_values'

--- templates/message_module.rs
+++ templates/message_module.rs
@@ -2,3 +2,40 @@
 {% for definition in defs %}
 pub mod {{ definition.fourcc | lower }};
 {% endfor %}
+
+use std::collections::HashMap;
+
+/// Attempt to read Message body based on it's fourcc and convert to a CSV line of scaled, unit-converted values
+pub fn to_csv(fourcc: [u8; 4], body: Vec<u8>) -> Option<String> {
+    match &fourcc {
+        {% for definition in defs %}
+        b"{{ definition.fourcc }}" => {
+            let data_raw = {{ definition.fourcc | lower }}::read_raw(body);
+            let data_scaled = {{ definition.fourcc | lower }}::{{ definition.fourcc }}::from_raw(data_raw);
+            Some(data_scaled.to_csv_line())
+        },
+        {% endfor %}
+        _ => {
+            println!("Skipped type {}", String::from_utf8_lossy(&fourcc));
+            None
+        },
+    }
+}
+
+
+/// Attempt to read Message body based on it's fourcc and convert to a map of scaled, unit-converted values
+pub fn to_values(fourcc: [u8; 4], body: Vec<u8>) -> Option<HashMap<String, f64>> {
+    match &fourcc {
+        {% for definition in defs %}
+        b"{{ definition.fourcc }}" => {
+            let data_raw = {{ definition.fourcc | lower }}::read_raw(body);
+            let data_scaled = {{ definition.fourcc | lower }}::{{ definition.fourcc }}::from_raw(data_raw);
+            Some(data_scaled.to_dict())
+        },
+        {% endfor %}
+        _ => {
+            println!("Skipped type {}", String::from_utf8_lossy(&fourcc));
+            None
+        },
+    }
+}
