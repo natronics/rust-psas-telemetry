3dbba2d2eb6b042d4b0f480dd059b100701a57b6
Nathan Bergey <nathan.bergey@gmail.com>
2017-12-31 20:01:15 -0700

    Filestats: Read a telemetry file and get the stats about its messages

--- templates/message_module.rs
+++ templates/message_module.rs
@@ -4,6 +4,58 @@ pub mod {{ definition.fourcc | lower }};
 {% endfor %}
 
 use std::collections::HashMap;
+use std::io::prelude::*;
+use std::io::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Cursor;
+
+use byteorder::{ReadBytesExt, BigEndian};
+
+
+// The `SEQN` header is part of the spec, and not defined with the TOML files.
+
+/// A message header
+pub struct Header {
+    /// The four character code, determines the type of message
+    pub fourcc: [u8; 4],
+
+    /// Time received in nanoseconds from message epoch
+    pub timestamp: u64,
+
+    /// The size of the upcoming message body, in bytes
+    pub size: usize,
+}
+
+
+/// Read from a buffer assuming were at the boundary of a message.
+/// Extracts the header
+pub fn read_header(reader: &mut BufReader<File>) -> Result<Header, Error> {
+    let mut fourcc_buf = [0; 4];
+    let mut header_buf = [0; 8];
+
+    let _char_read = reader.read_exact(&mut fourcc_buf)?;
+    let _char_read = reader.read_exact(&mut header_buf)?;
+    let mut header_cursor = Cursor::new(&header_buf);
+
+    let timestamp_hi = header_cursor.read_u16::<BigEndian>().unwrap() as u64;
+    let timestamp_lo = header_cursor.read_u32::<BigEndian>().unwrap() as u64;
+    let message_length = header_cursor.read_u16::<BigEndian>().unwrap() as usize;
+
+    let timestamp: u64 = timestamp_hi << 32 | timestamp_lo;
+
+    Ok(Header {
+        fourcc: fourcc_buf,
+        timestamp: timestamp,
+        size: message_length,
+    })
+}
+
+pub fn read_seqn_body(body: Vec<u8>) -> u32 {
+    let mut body = Cursor::new(&body);
+    body.read_u32::<BigEndian>().unwrap()
+}
+
 
 /// Attempt to read Message body based on it's fourcc and convert to a CSV line of scaled, unit-converted values
 pub fn to_csv(fourcc: [u8; 4], body: Vec<u8>) -> Option<String> {
