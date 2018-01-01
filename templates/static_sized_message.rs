992a9452c41ff4780fc369894239e0e017240a4e
Nathan Bergey <nathan.bergey@gmail.com>
2017-12-31 17:29:17 -0700

    Codegen: template code for reading a message, impl 'from_raw', 'to_csv_line', and 'to_dict'

--- templates/static_sized_message.rs
+++ templates/static_sized_message.rs
@@ -1,4 +1,10 @@
 /* This file is auto generated! Do not edit. */
+/*!
+Module for the {{ name }} telemetry messages.
+*/
+use std::io::Cursor;
+use byteorder::{ReadBytesExt, BigEndian};
+use std::collections::HashMap;
 
 
 /// {{ description }}
@@ -13,7 +19,6 @@ pub struct {{ fourcc }}_raw {
 
 
 /// {{ description }}
-#[allow(non_camel_case_types)]
 pub struct {{ fourcc }} {
     {% for field in member %}
     /// {{ field.description }} [{{ field.units.mks }}]
@@ -22,3 +27,54 @@ pub struct {{ fourcc }} {
 }
 
 
+impl {{ fourcc }} {
+
+    /// Convert a {{ fourcc }}_raw struct to a converted units struct by applying scaleing
+    pub fn from_raw(raw: {{ fourcc }}_raw) -> {{ fourcc }} {
+        {{ fourcc }} {
+            {% for field in member %}
+            {{ field.key | lower }}: raw.{{ field.key | lower }} as f64 {% if field.units.scaleby %} * {{ field.units.scaleby }}{% endif %},
+            {% endfor %}
+        }
+    }
+
+    /// Convert values to a single line suitable for a CSV file
+    /// (ex: "12.0,-15.2,1352.8123,")
+    pub fn to_csv_line(&self) -> String {
+        let mut line = String::new();
+
+        {% for field in member %}
+        line += &format!("{},", self.{{ field.key | lower }});
+        {% endfor %}
+
+        line
+    }
+
+    /// convert values to a dictionary where the key is a string
+    /// (ex: {'value_1': 12.0, 'value_2': -15.2})
+    pub fn to_dict(&self) -> HashMap<String, f64> {
+        let mut map = HashMap::new();
+
+        {% for field in member %}
+        map.insert("{{ field.key }}".to_owned(), self.{{ field.key | lower }});
+        {% endfor %}
+
+        map
+    }
+}
+
+
+/// Read the body and return unpacked struct of raw values
+pub fn read_raw(body: Vec<u8>) -> {{ fourcc }}_raw {
+    let mut cursor = Cursor::new(body);
+
+    {{ fourcc }}_raw {
+        {% for field in member %}
+        {% if field.type == "u8" %}
+            {{ field.key | lower }}: cursor.read_{{ field.type }}().unwrap(),
+        {% else %}
+            {{ field.key | lower}}: cursor.read_{{ field.type }}::<BigEndian>().unwrap(),
+        {% endif %}
+        {% endfor %}
+    }
+}
